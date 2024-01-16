da851cc908b0edc3f0c68b047e06744d2928dba2
munair <munair@gmail.com>
2024-01-16 10:37:34 +0900

    implement variable names for folder ids

--- listfolderspreadsheets/listfolderspreadsheets.js
+++ listfolderspreadsheets/listfolderspreadsheets.js
@@ -22,5 +22,7 @@ async function listSpreadsheetsInFolder(folderId) {
     }
 }
 
-listSpreadsheetsInFolder('1z38V8POr9lXNAoFBM-7I5_8GG9t5E2wx');
+const realdata = '1k0FhOtK-3_mGoH5CnC9axY2l2FsP7h1q'; // Variable containing the identifier of the folder containing 'real trades'.
+const fakedata = '1z38V8POr9lXNAoFBM-7I5_8GG9t5E2wx'; // Variable containing the identifier of the folder containing 'fake trades'.
+listSpreadsheetsInFolder(fakedata);
 
